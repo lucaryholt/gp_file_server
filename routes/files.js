ceadc7376344a3731f067369687d7002406cf4d4
Luca Ryholt <lucaryholt@gmail.com>
2020-11-09 15:39:22 +0100

    updated README.md

--- routes/files.js
+++ routes/files.js
@@ -3,10 +3,18 @@ const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
 
+let upload = null;
+
 const multer = require('multer');
-const upload = multer({
-    dest: path.join(__dirname, process.env.UPLOAD_DIRECTORY)
-});
+if (process.env.UPLOAD_DIRECTORY_ABSOLUTE_PATH) {
+    upload = multer({
+        dest: process.env.UPLOAD_DIRECTORY_ABSOLUTE_PATH
+    });
+} else {
+    upload = multer({
+        dest: path.join(__dirname, '..', process.env.UPLOAD_DIRECTORY_RELATIVE_PATH)
+    });
+}
 
 const auth = require('./auth.js');
 
